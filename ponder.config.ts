b34e6bda675e78db5f9d1b96672075284128c9d6
Rafael Sanches <rafaeldss12@gmail.com>
2024-05-31 15:06:19 -0300

    uncommented kakarot network

--- ponder.config.ts
+++ ponder.config.ts
@@ -9,10 +9,10 @@ export default createConfig({
       chainId: 11155111,
       transport: http(process.env.ALCHEMY_RPC_URL),
     },
-    // kakarot: {
-    //   chainId: 1802203764,
-    //   transport: http(process.env.KAKAROT_RPC_URL),
-    // },
+    kakarot: {
+      chainId: 1802203764,
+      transport: http(process.env.KAKAROT_RPC_URL),
+    },
   },
   contracts: {
     Swaplace: {
@@ -22,10 +22,10 @@ export default createConfig({
           address: "0xFA682bcE8b1dff8D948aAE9f0fBade82D28E1842",
           startBlock: 5719042,
         },
-        // kakarot: {
-        //   address: "0xB317127b50b22e62637E3c333A585a8ccfd0721D",
-        //   startBlock: 90,
-        // },
+        kakarot: {
+          address: "0xB317127b50b22e62637E3c333A585a8ccfd0721D",
+          startBlock: 90,
+        },
       },
     },
   },
